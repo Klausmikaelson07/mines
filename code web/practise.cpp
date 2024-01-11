217d01acdd6acfa2b83f3d16c172577e972ff291
Klausmikaelson07 <156284802+Klausmikaelson07@users.noreply.github.com>
2024-01-11 08:20:14 +0200

    /code web/index.html
    project

---
+++ code web/practise.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+    int count=0;
+    while(count<5)
+    {
+        cout<<"Hello World \n";
+        count=count+1; 
+    }
+    return 0;
+}
